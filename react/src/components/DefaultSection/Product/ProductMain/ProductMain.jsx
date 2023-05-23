7559129d9266034fe103cbd3299b4a070b5bc3e9
baco0611 <baoblink@gmail.com>
2023-05-23 13:35:05 +0700

    [CREATE, FIX] Character, Loader
    Create Loader Component and add to Explore and Introduce
    Create Character Section

--- react/src/components/DefaultSection/Product/ProductMain/ProductMain.jsx
+++ react/src/components/DefaultSection/Product/ProductMain/ProductMain.jsx
@@ -1,3 +1,4 @@
+import Character from './Character/Character'
 import Explore from './Explore/Explore'
 import Introduce from './Introduce/Introduce'
 import './ProductMain.scss'
@@ -7,6 +8,7 @@ function ProductMain({ slug }) {
         <div id='main-section'>
             <Introduce slug={slug}/>
             <Explore slug={slug}/>
+            <Character slug={slug}/>
         </div>
     )
 }
