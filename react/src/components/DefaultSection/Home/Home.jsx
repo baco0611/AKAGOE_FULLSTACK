e46c065de40115ed33449b32f456a3f26f2b244a
baco0611 <baoblink@gmail.com>
2023-05-26 11:55:22 +0700

    [FIX] Fix onscroll bug (delete feature, add later)

--- react/src/components/DefaultSection/Home/Home.jsx
+++ react/src/components/DefaultSection/Home/Home.jsx
@@ -12,25 +12,25 @@ function Home() {
     const { setThemeColor } = useContext(HomeSectionContext)
     setThemeColor('default')
 
-    window.onscroll = () => {
-        console.log(testRef.current.getBoundingClientRect())
-        const elementRect = testRef.current.getBoundingClientRect()
-        // console.log(testRef.current.scrollWidth)
-        console.log(window.scrollY)
-        console.log(windowY, elementRect.y)
-        // console.log(document.documentElement.offsetTop)
+    // window.onscroll = () => {
+    //     console.log(testRef.current.getBoundingClientRect())
+    //     const elementRect = testRef.current.getBoundingClientRect()
+    //     // console.log(testRef.current.scrollWidth)
+    //     console.log(window.scrollY)
+    //     console.log(windowY, elementRect.y)
+    //     // console.log(document.documentElement.offsetTop)
 
-        if(elementRect.y <= 0 && elementRect.y >= 0) {
-            setWindowY(window.scrollY)
-        } else setWindowY(0)
+    //     if(elementRect.y <= 0 && elementRect.y >= 0) {
+    //         setWindowY(window.scrollY)
+    //     } else setWindowY(0)
 
-        if(elementRect.y <= 0 && elementRect.y >= -elementRect.height) {
-            window.onscroll = () => window.scroll(0,0)
-            setStyle({
-                transform: `translate(-541.611px, 0)`
-            })
-        } else setStyle({})
-    }
+    //     if(elementRect.y <= 0 && elementRect.y >= -elementRect.height) {
+    //         window.onscroll = () => window.scroll(0,0)
+    //         setStyle({
+    //             transform: `translate(-541.611px, 0)`
+    //         })
+    //     } else setStyle({})
+    // }
 
     // window.onscroll = () => window.scroll(0,0)
     return (
