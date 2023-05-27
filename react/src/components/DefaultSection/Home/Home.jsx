86e5daf1a1bd31649c4116f0546edd022aaf1f51
baco0611 <baoblink@gmail.com>
2023-05-27 23:59:29 +0700

    [FIX, CREATE] FIX HOME SECTION AND CREATE SECTION1 COMPONENT
    Use window.innerWidth to handle window, don't use screen.width

--- react/src/components/DefaultSection/Home/Home.jsx
+++ react/src/components/DefaultSection/Home/Home.jsx
@@ -1,11 +1,13 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import './Home.scss'
 import { HomeSectionContext } from '../../../context/HomeProvider';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../../../views/Loader/Loader';
 import axios from 'axios';
-import Section1 from './Section1';
+import Section1 from './Section/Section1/Section1';
+import Title from './Title/Title';
+import Section2 from './Section/Section2/Section2';
 
 function Home() {
     
@@ -14,10 +16,8 @@ function Home() {
     const navigate = useNavigate()
     const { setThemeColor } = useContext(HomeSectionContext)
     const [translate, setTranslate] = useState(0)
-    const width = 2 * screen.width + screen.height
-    // alert(width)
-    
-    console.log(translate, window.scrollY)
+    const [width, setWidth] = useState(2 * window.innerWidth + window.innerHeight) 
+
 
     useEffect(() => {
         //fetch API
@@ -42,10 +42,20 @@ function Home() {
 
     useEffect(() => {
         const handleScroll = () => {
-            if(window.scrollY <= width)
+
+            const stopScroll = (e) => {
+                e.preventDefault();
+            }
+
+            if(window.scrollY <= (2*screen.width + screen.height))
+            {
                 setTranslate(window.scrollY)
+                window.removeEventListener('scroll', stopScroll)
+            }
             else
-                setTranslate(width)
+            {
+                setTranslate(2*screen.width)
+            }
         }
 
         window.addEventListener('scroll', handleScroll)
@@ -62,10 +72,11 @@ function Home() {
         <>
             <div id='home-section'>
                 <div className='container'>
+                    <Title/>
                     <div className='camera' style={{transform: `translate3d(-${translate}px, 0 ,0)`}}>
                         <Section1 urlImage={homeData.image1}/>
-                        <Section1 urlImage={homeData.image1}/>
-                        <Section1 urlImage={homeData.image1}/>
+                        <Section2 urlImage={homeData.image2}/>
+                        {/* <Section2 urlImage={homeData.image2}/> */}
                     </div>
                 </div>
                 <div style={{width: '100%', height:`${width}px`}}></div>
