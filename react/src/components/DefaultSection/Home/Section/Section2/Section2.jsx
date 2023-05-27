baa7c5661d6a1cc3951a3cdb1da817954e87ed91
baco0611 <baoblink@gmail.com>
2023-05-28 02:23:59 +0700

    [ADD] Section 2

--- react/src/components/DefaultSection/Home/Section/Section2/Section2.jsx
+++ react/src/components/DefaultSection/Home/Section/Section2/Section2.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import clsx from 'clsx'
+import './Section2.scss'
 
 function Section2({ urlImage}) {
 
     const [distance, setDistance] = useState(0)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
     // Set the scroll Y of web
     useEffect(() => {
@@ -12,15 +15,58 @@ function Section2({ urlImage}) {
 
         window.addEventListener('scroll', handleChangDistance)
 
+        window.onresize = () => setWindowWidth(window.innerWidth)
+ 
         return () => {
             window.removeEventListener('scroll', handleChangDistance)
         }
     }, [])
 
     return(
-        <section id="section-2">
-            <img className="main-img" src={urlImage}/>
-
+        <section id="section-2" style={{
+            opacity: 1 - (windowWidth-distance)/windowWidth
+        }}>
+            {
+                distance-windowWidth>-(windowWidth/2.75)
+                ?
+                    <img className="main-img active-img" src={urlImage}/>
+                :
+                    <img className="main-img" src={urlImage}/>
+                
+            }
+            <div className="section-2-menu"></div>
+            <div className="section-2-main">
+                <div className="section-2-content">
+                    {
+                        distance-windowWidth>-(windowWidth/2.75)
+                        ?
+                        <div className=" section-2-mission active-mission">
+                            <h3>KIS-GE connects Vietnam and Japan, trains quality human resources, and becomes a young leader promoting bilateral economic development.</h3>
+                            <h1>MISSION</h1>
+                        </div>
+                        :
+                        <div className=" section-2-mission">
+                            <h3>KIS-GE connects Vietnam and Japan, trains quality human resources, and becomes a young leader promoting bilateral economic development.</h3>
+                            <h1>MISSION</h1>
+                        </div>
+                    }
+                </div>
+                <div className="section-2-content">
+                    {
+                        distance-windowWidth>-(windowWidth/2.75)
+                        ?
+                        <div className=" section-2-vision active-vision">
+                            <h1>VISION</h1>
+                            <h3>KIS-GE is a company that attracts 100 companies and corporations from Japan that intend to invest in Vietnam by 2030. With a short-term vision, within 10 years, it can train high-quality personnel in Vietnam.</h3>
+                        </div>
+                        :
+                        <div className=" section-2-vision">
+                            <h1>VISION</h1>
+                            <h3>KIS-GE is a company that attracts 100 companies and corporations from Japan that intend to invest in Vietnam by 2030. With a short-term vision, within 10 years, it can train high-quality personnel in Vietnam.</h3>
+                        </div>
+                    }
+                </div>
+            </div>
         </section>
     )
 }
