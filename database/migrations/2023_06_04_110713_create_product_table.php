2fe08b3f62969a9b13f47a221ee519830771200d
your name <ntvanh2003a2@gmail.com>
2023-06-04 18:31:12 +0700

    [CREATE] create all table for shop

---
+++ database/migrations/2023_06_04_110713_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id('id_product');
+            $table->string('product_name');
+            $table->integer('quantity');
+            $table->integer('sale_quantity')->nullable();
+            $table->integer('price');
+            $table->string('product_description')->nullable();
+            $table->string('color')->nullable();
+            $table->string('size')->nullable();
+            $table->foreignId('id_supplier')->nullable();//dev sau
+            $table->foreignId('id_product_type');
+            $table->foreign('id_product_type')->references('id_product_type')->on('product_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
